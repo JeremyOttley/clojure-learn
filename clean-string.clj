ba6bac12a8769a72f152dffa623373d16ab3a7bb
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-20 09:16:49 -0500

    Create clean-string.clj

---
+++ clean-string.clj
@@ -0,0 +1,10 @@
+;;;; we’re defining a function that’ll help us clean the text input from these files. We’re taking in that ;;;; string that we slurped, and piping it through the following transformations:
+;; 1. Lowercasing the string
+;; 2. Replacing line breaks with spaces
+;; 3. Removing special characters (because some people write lyrics like “Yeah!!!!!!!!!”)
+
+(defn clean-string [s]
+  (-> s
+      (clojure.string/lower-case)
+      (clojure.string/replace "n" " ")
+      (clojure.string/replace #"[^A-Za-z0-9 ]" "")))
