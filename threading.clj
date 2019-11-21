20b8e1670ab99fc9af28bd7d121afe5d0e0a16f9
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-21 13:42:03 -0500

    Update threading.clj

--- threading.clj
+++ threading.clj
@@ -1,3 +1,9 @@
+; When you are in a situation where you want more freedom as where to
+; put the result of previous data transformations in an 
+; expression, you can use the as-> macro. With it, you can assign a
+; specific name to transformations' output and use it as a
+; placeholder in your chained expressions:
+
 (def coll (list 1 2 3))
 
 (reduce + (remove nil? coll))
