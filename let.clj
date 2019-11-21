d2c322b1cc367b11752fdae90a7e78ae1f36bb14
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-21 18:14:19 -0500

    Create let.clj

---
+++ let.clj
@@ -0,0 +1,6 @@
+(ns let-fn-example)
+
+(let [name1     "Maxwell"
+      name2    "Gillian"
+      name3 "Jeremy"]
+  (println (str "I was with " name1 " and " name2 " but " name3 " wasn't there.")))
