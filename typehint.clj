ba03e9c4847c201daab38ca0855a47303a9836e8
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-23 15:59:04 -0500

    Create typehint.clj

---
+++ typehint.clj
@@ -0,0 +1,4 @@
+;; Type hints (Metadata) are used to tell the compiler what type the value will be 
+;; and allow it to perform type specific optimisations thus potentially making resultant code a bit faster.
+
+(def ^Integer five 5)
