e344f2fe888919194452a0bd359fe5a094a29931
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-24 15:06:31 -0500

    Create heredoc.clj

---
+++ heredoc.clj
@@ -0,0 +1,7 @@
+(defn heredoc [& strings] (clojure.string/join "\n" strings))
+
+
+
+;;(heredoc "This is the first line. Implicitly I want a new line"
+;;          "When I put a new line in the function call to create a new line")
+
