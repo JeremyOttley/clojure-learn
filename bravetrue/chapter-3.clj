69cd418422ca335dbc70538f90eb532e9a6d7352
Jeremy Ottley <Gazaunga@gmail.com>
2020-02-17 10:36:34 -0500

    Update chapter-3.clj

--- bravetrue/chapter-3.clj
+++ bravetrue/chapter-3.clj
@@ -2,3 +2,7 @@
   "Write a function that takes a number and adds 100 to it."
   (+ 100 n))
 
+(defn dec-maker
+ "Create a custom decrementor"
+ [dec-by]
+ #(- % dec-by)) ;(def dec3 (dec-maker 3)) -> (dec3 6) = 3
