250243f074aaadeefe1e4cdfffc6b191619a3f3c
Jeremy Ottley <Gazaunga@gmail.com>
2020-04-12 16:05:34 -0400

    Create guess.clj

---
+++ guess.clj
@@ -0,0 +1,16 @@
+(defn try-and-guess [number]
+  (println "Enter your guess:")
+  (let [guess (Integer/parseInt (read-line))]
+    (cond
+      (< guess number) 
+        (do (println "Guess is too small...")
+            (try-and-guess number))
+      (> guess number)
+        (do (println "Guess is too big...")
+            (try-and-guess number))
+      :else
+      (println "Yeah!"))))
+
+(defn -main []
+  (let [number (rand-int 100)]
+    (try-and-guess number)))
