5af14b323caaff6a86aac8598438863a0d45fec5
Jeremy Ottley <Gazaunga@gmail.com>
2020-04-16 12:44:37 -0400

    Update reagent.cljs

--- reagent.cljs
+++ reagent.cljs
@@ -39,3 +39,9 @@
                             :count shared-count}]
                   [counter {:heading "I'm C, and I'm independent"
                             :count 0}]] js/klipse-container))
+
+;; When using reagent, I like to always use local state for my components, by having them return a fn, and wrapping it in a let binding, the state can be encapsulated with the component.
+
+;; I also like to have my components take a state map, whose values are either a ratom or an initial value. This way, if you don't need the state of the component outside, you just create the state map with initial values. If you do need the state outside, say to share it, like in my example, then you can pass in ratoms for the state values you want shared/exposed outside.
+
+;; This allows me to use the components in many ways, like how I have two counter that are synchronized, sharing count, yet with independent headings, and one counter that's fully independent.
