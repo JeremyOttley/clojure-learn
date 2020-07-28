a92c00624c0cb65f8a7abe6937a1b2ab81f533ea
Jeremy Ottley <Gazaunga@gmail.com>
2020-07-28 09:48:47 -0400

    Create server.clj

---
+++ server.clj
@@ -0,0 +1,26 @@
+;; When executed, this file will run a basic web server
+;; on http://localhost:8080.
+
+(ns ring.example.params
+  (:use ring.middleware.params
+        ring.util.response
+        ring.adapter.jetty))
+
+(defn page [name]
+  (str "<html><body>"
+       (if name
+         (str "Nice to meet you, " name "!")
+         (str "<form>"
+              "Name: <input name='name' type='text'>"
+              "<input type='submit'>"
+              "</form>"))
+       "</body></html>"))
+
+(defn handler [{{name "name"} :params}]
+  (-> (response (page name))
+      (content-type "text/html")))
+
+(def app
+  (-> handler wrap-params))
+
+(run-jetty app {:port 8080})
