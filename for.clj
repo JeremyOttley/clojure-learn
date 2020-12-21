9211cc0e914a19fc9ddf137161d6b7c3e96b6c51
Jeremy Ottley <Gazaunga@gmail.com>
2020-12-21 14:32:45 -0500

    Update for.clj

--- for.clj
+++ for.clj
@@ -1,2 +1,8 @@
 ;; produces a list of numbers 1 through 20
 (for [i (range 20)] (inc i))
+
+;; for is doseq that produces a seq
+
+;; prepare a seq of all keys from entries whose values are 0
+(for [[x y] '([:a 1] [:b 2] [:c 0]) :when (= y 0)] x)
+;;=> (:c)
