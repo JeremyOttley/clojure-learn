8f3f58a7e5b5625562d726f8f8e7e89faf9f8e5f
Jeremy Ottley <Gazaunga@gmail.com>
2021-01-10 07:35:57 -0500

    Update super-print.clj

--- super-print.clj
+++ super-print.clj
@@ -3,7 +3,7 @@
   ([s c]
    (let [margin 4
          length (+ (* 2 margin) (count s))]
-     (println (string/join "" (repeat length c)))
+     (println (clojure.string/join "" (repeat length c)))
      (println (str c c "  " s "  " c c))
-     (println (string/join "" (repeat length c)))
+     (println (clojure.string/join "" (repeat length c)))
      (println ""))))
