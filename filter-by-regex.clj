2e0509f16f5d7be6f9607523221ce32b0c7d365f
Jeremy Ottley <Gazaunga@gmail.com>
2021-07-30 14:07:18 -0400

    Update filter-by-regex.clj

--- filter-by-regex.clj
+++ filter-by-regex.clj
@@ -6,4 +6,13 @@
   [regex sequence] 
     (filter #(re-matches regex %) sequence))
 
-(filter-by-regex #"\w{3}" ["Vim" "Emacs" "VSCode" "Intellij"])
+(filter-by-regex #"\w{3}" ["Vim" "Emacs" "VSCode" "Intellij"]) ;; ("Vim")
+
+;; or
+
+(defn filter-by-regex 
+  [regex sequence] 
+    (filter #(re-find regex %) sequence))
+
+
+(filter-by-regex #"i" ["Vim" "Emacs" "VSCode" "Intellij"]) ;; ("Vim" "Intellij")
